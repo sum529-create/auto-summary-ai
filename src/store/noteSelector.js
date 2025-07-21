d49c698f052d1cbb860b1c7484a04059532efd6d
노수민 <nosumin29@gmail.com>
2025-07-21 16:07:10 +0900

    fix: 함수의 명확성을 위한 noteSelector 코드 정리 #3

--- src/store/noteSelector.js
+++ src/store/noteSelector.js
@@ -1,4 +1,3 @@
 export const selectNoteById = (id) => (state) => {
-  const note = state.notes.find(e => e.id.toString() === id.toString());
-  if(note) return note;
+  return state.notes.find(e => e.id.toString() === id.toString());
 }
