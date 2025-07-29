d7b4e52fcba47c30582300de04669ca1c4d7d2ab
노수민 <nosumin29@gmail.com>
2025-07-29 16:31:49 +0900

    style: FlexRow 텍스트 overflow시 자동 영역 맞춤 #4

--- src/components/notes/NoteListItem.jsx
+++ src/components/notes/NoteListItem.jsx
@@ -7,13 +7,13 @@ const NoteListItem = ({data}) => {
     <li className='cursor-pointer bg-sumi-moon p-4 rounded'>
       <Link to={`/notes/${data.id}`}>
         <FlexRow>
-          <div>
+          <div className="flex-1 min-w-0">
             <h3 className='text-gray-900 text-lg'>{data.title}</h3>
             <p className='text-gray-700 text-base max-w-full min-w-0 flex-grow overflow-hidden text-ellipsis whitespace-nowrap'>
               {data.content.slice(0, 50)}
             </p>
           </div>
-          <div className='text-gray-500 text-sm'>{data.date}</div>
+          <div className='text-gray-500 text-sm whitespace-nowrap'>{data.date}</div>
         </FlexRow>
       </Link>
     </li>
