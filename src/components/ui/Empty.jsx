26ae2536e4d007846a0bea4c7b4699b2d9ef5eb3
노수민 <nosumin29@gmail.com>
2025-07-29 16:52:18 +0900

    fix: tailwind 클래스명 오타 수정 #4

--- src/components/ui/Empty.jsx
+++ src/components/ui/Empty.jsx
@@ -7,7 +7,7 @@ const Empty = () => {
   return (
     <div className='text-center space-y-2'>
       <h3 className='text-white text-2xl'>Empty</h3>
-      <p className='text-gray-300 text-bas !my-5'>노트를 만들어보세요</p>
+      <p className='text-gray-300 text-base !my-5'>노트를 만들어보세요</p>
       <Button onClick={() => handleClick(navigate)} variant="light" className="my-4">노트 작성</Button>
     </div>
   )
