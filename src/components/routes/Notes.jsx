02b0f79f2de579471ee955e2218993f2f446807f
노수민 <nosumin29@gmail.com>
2025-07-29 16:51:28 +0900

    fix: Empty 컴포넌트 import 경로의 대소문자 수정 #4

--- src/components/routes/Notes.jsx
+++ src/components/routes/Notes.jsx
@@ -2,7 +2,7 @@ import NoteList from "../notes/NoteList";
 import NoteArea from "../notes/NoteArea";
 import SortedArea from "../notes/SortedArea";
 import { useSelector } from "react-redux";
-import Empty from "../ui/empty";
+import Empty from "../ui/Empty";
 
 const Notes = () => {
   const notes = useSelector(state => state.notes);
