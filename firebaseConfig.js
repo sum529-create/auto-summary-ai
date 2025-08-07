f06df167e743c20c07f172fbdc60240f66dca0a0
노수민 <nosumin29@gmail.com>
2025-08-07 16:21:48 +0900

    feat: Firebase 배포 진행 #5

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3LPXZ3Ji01gTfI4QjBMc1gV7rdrcvgFk",
+  authDomain: "auto-summary-ai.firebaseapp.com",
+  projectId: "auto-summary-ai",
+  storageBucket: "auto-summary-ai.firebasestorage.app",
+  messagingSenderId: "409823949320",
+  appId: "1:409823949320:web:09781ccce83dba253ed7bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
